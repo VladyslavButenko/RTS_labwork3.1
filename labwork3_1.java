0d2190224dd89ff7ed767776f4adf2ceed6673ff
Vladyslav Butenko <vbutenko713@gmail.com>
2020-04-25 18:48:06 +0300

    Commit 1: Completed Additional Task (вивести час виконання через pop-up)

--- labwork3_1.java
+++ labwork3_1.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
+    private long startTime;
+    private long endTime;
 
     @Override
     void onCreate(Bundle savedInstanceState) {
@@ -18,19 +20,28 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_lab1);
 
         Button calcButton = (Button) findViewById(R.id.calcButtonl1);
+
         final EditText numbEditText = (EditText) findViewById(R.id.numbEditText);
         final TextView resTextView = (TextView) findViewById(R.id.resTextViewl1);
+        final TextView popUpView = (TextView) findViewById(R.id.popUpView);
 
         calcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialogBuilder builder = new AlertDialog.Builder(Main.this)
+                builder.setCancelable(true);
+                builder.setTitle("Total Execution Time");
                 try {
                     int number = Integer.parseInt(numbEditText.getText().toString());
                     if (isPrime(number)) resTextView.setText("Prime");
                     else if (number % 2 == 0) resTextView.setText("Even");
                     else {
+                        startTime = System.nanoTime();
                         int resArray[] = calculateFerma(number);
+                        endTime = (System.nanoTime() - startTime)/1_000;
+                        builder.setMessage("Total execution time: " + endTime + " ms");
                         resTextView.setText(String.format("A = %d, B = %d\nA * B = %d", resArray[0], resArray[1], number));
+                        builder.show();
                     }
                 } catch (Exception e) {
                     resTextView.setText("Unsigned integer must be entered!");
